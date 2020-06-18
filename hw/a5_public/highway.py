af83f80936083ca233a67f30d594e4f421bcaf06
FlyingPig <1800012735@pku.edu.cn>
2020-06-18 22:57:11 +0800

    first commit

---
+++ hw/a5_public/highway.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+CS224N 2019-20: Homework 5
+"""
+
+import torch
+import torch.nn as nn
+
+class Highway(nn.Module):
+    pass
+    # Remember to delete the above 'pass' after your implementation
+    ### YOUR CODE HERE for part 1f
+
+
+
+
+    ### END YOUR CODE
+
